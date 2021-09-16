0c6b5a57a1d4887e6ff238e41434e1096af41b18
Michael Thompson <michael.99.thompson@gmail.com>
2021-09-16 17:07:32 -0400

    Two Player PONG is working better.

--- diagnostics/14-Sliders-OLEDs.py
+++ diagnostics/14-Sliders-OLEDs.py
@@ -1,18 +1,14 @@
+#RICM Micro PONG
+
 import machine
 from machine import Pin, PWM
 import utime
 import random
 import OLED
-#from machine import Pin, PWM
-#from utime import sleep
 
-#from machine import ADC, Pin      # Get the ADC and Pin libraries
-
-#displays the display ID on the OLED screens
-#tests: OLED module 1
+#Assumes that the sliders, OLEDS, and speaker are working
 
 paddle_width = 20
-paddle_color = 1
 paddle1_x = 50
 paddle1_y = 63
 paddle2_x = 50
@@ -24,12 +20,14 @@ ball_y = 20
 ball_y_increase = True
 ball_height = 5
 ball_width = 5
-ball_color = 1
+ball_screen = 2
+white = 1
+black = 0
 
 oled_height = 64
 oled_width = 128
 
-# create a Pulse Width Modulation Object on this pin
+#Create a Pulse Width Modulation Object on this pin
 speaker_pin = 7
 speaker = PWM(Pin(speaker_pin))
 tones = { "B0": 31,"C1": 33,"CS1": 35,"D1": 37,"DS1": 39,"E1": 41,"F1": 44,"FS1": 46, "G1": 49,"GS1": 52,"A1": 55,"AS1": 58,"B1": 62,"C2": 65, "CS2": 69,"D2": 73,"DS2": 78,"E2": 82,"F2": 87,"FS2": 93,"G2": 98, "GS2": 104,"A2": 110,"AS2": 117,"B2": 123,"C3": 131,"CS3": 139, "D3": 147,"DS3": 156,"E3": 165,"F3": 175,"FS3": 185, "G3": 196,"GS3": 208,"A3": 220,"AS3": 233,"B3": 247,"C4": 262,"CS4": 277,"D4": 294,"DS4": 311, "E4": 330,"F4": 349,"FS4": 370,"G4": 392,"GS4": 415,"A4": 440,"AS4": 466,"B4": 494,"C5": 523,"CS5": 554,"D5": 587,"DS5": 622,"E5": 659,"F5": 698, "FS5": 740,"G5": 784,"GS5": 831,"A5": 880,"AS5": 932,"B5": 988,"C6": 1047,"CS6": 1109,"D6": 1175,"DS6": 1245,"E6": 1319,"F6": 1397,"FS6": 1480,"G6": 1568,"GS6": 1661, "A6": 1760,"AS6": 1865,"B6": 1976,"C7": 2093,"CS7": 2217,"D7": 2349,"DS7": 2489,"E7": 2637,"F7": 2794,"FS7": 2960,"G7": 3136,"GS7": 3322,"A7": 3520, "AS7": 3729,"B7": 3951,"C8": 4186,"CS8": 4435,"D8": 4699,"DS8": 4978 }
@@ -119,15 +117,24 @@ else:
         ball_y_increase = True
 
         while(True):
-            oled1.fill(0)
-            oled1.hline(oled_width - paddle_width - paddle1_x,paddle1_y,paddle_width,paddle_color)
-            oled1.rect(ball_x,ball_y,ball_width,ball_height,ball_color)
-            oled1.show()
-            
-            oled2.fill(0)
-            oled2.hline(paddle2_x,paddle2_y,paddle_width,paddle_color)
-            oled2.rect(ball_x,ball_y,ball_width,ball_height,ball_color)
-            oled2.show()
+            if ball_screen == 1:
+                oled1.fill(0)
+                oled1.hline(oled_width - paddle_width - paddle1_x,paddle1_y,paddle_width,white)
+                oled1.rect(ball_x,ball_y,ball_width,ball_height,white)
+                oled1.show()
+                
+                oled2.fill(0)
+                oled2.hline(paddle2_x,paddle2_y,paddle_width,white)
+                oled2.show()
+            else:
+                oled1.fill(0)
+                oled1.hline(oled_width - paddle_width - paddle1_x,paddle1_y,paddle_width,white)
+                oled1.show()
+
+                oled2.fill(0)
+                oled2.hline(paddle2_x,paddle2_y,paddle_width,white)
+                oled2.rect(ball_x,ball_y,ball_width,ball_height,white)
+                oled2.show()
 
             #Ball at right side of screen
             if ball_x == (oled_width - ball_height):
@@ -173,10 +180,16 @@ else:
 
             #Ball at to of screen
             if ball_y == 0:
-                ball_y_increase = True
-                playtone(tones["A5"])
-                utime.sleep_ms(30)
-                bequiet()
+                if ball_screen == 1:
+                    ball_screen = 2
+                else:
+                    ball_screen = 1
+
+                ball_y_increase = True #Change the ball Y direction
+                
+                ball_x_increase = not ball_x_increase #Change the ball X direction
+            
+                ball_x = oled_width - ball_x #The screens are rotated 180 degrees so we need adjust the X
 
             if ball_y_increase:
                 ball_y = ball_y + 1
